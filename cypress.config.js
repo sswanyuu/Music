fa2763e3095f6ce6ccb20b5272643f182daac112
Olgaa <yosoywanyushih@gmail.com>
2023-01-31 23:29:05 +0800

    formatting with ESLint and Prettier

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+//testing
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+    baseUrl: "http://localhost:4173",
+  },
+});
