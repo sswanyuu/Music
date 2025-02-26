7b4506a3d9eae292e752be39be1d2a9d59812a6b
Olgaa <yosoywanyushih@gmail.com>
2025-02-26 16:10:41 +1100

    feat: set up route

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: Home,
+  },
+  {
+    name: "about",
+    path: "/about",
+    component: About,
+  },
+  {
+    name: "manage",
+    path: "/manage",
+    component: Manage,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: { name: "home" },
+  },
+];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+  linkExactActiveClass: "text-yellow-500",
+});
 
-export default router
+export default router;
